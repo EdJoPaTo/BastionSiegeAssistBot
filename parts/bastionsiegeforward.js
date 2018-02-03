fec005b7269d59bd1e27ebe44086e0fa58f45568
Edgar Toll <edjopato@gmail.com>
2018-02-03 15:33:12 +0100

    feat: answer the send type of forwarded game screen

--- parts/bastionsiegeforward.js
+++ parts/bastionsiegeforward.js
@@ -2,9 +2,15 @@ const Telegraf = require('telegraf')
 
 const { Extra, Markup } = Telegraf
 
+const { detectgamescreen } = require('../lib/detectgamescreen')
+
 const bot = new Telegraf.Composer()
-module.exports = bot
 
 bot.on('text', ctx => {
-  return ctx.reply('test!!1!')
+  console.log('bastion siege message', ctx.message.text)
+  return ctx.reply(detectgamescreen(ctx.message))
 })
+
+const abstraction = new Telegraf.Composer()
+abstraction.use(Telegraf.optional(ctx => ctx && ctx.message && ctx.message.forward_from && ctx.message.forward_from.id === 252148344, bot))
+module.exports = abstraction
