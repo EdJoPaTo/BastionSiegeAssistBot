a30063e70126c4bcefa3916fc94f5e84ae8643cd
Edgar Toll <edjopato@gmail.com>
2018-02-03 16:39:00 +0100

    style: remove last , in object

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const localSession = new LocalSession({
   // Format of storage/database (default: JSON.stringify / JSON.parse)
   format: {
     serialize: (obj) => JSON.stringify(obj, null, 2), // null & 2 for pretty-formatted JSON
-    deserialize: (str) => JSON.parse(str),
+    deserialize: (str) => JSON.parse(str)
   },
   // We will use `messages` array in our database to store user messages using exported lowdb instance from LocalSession via Telegraf Context
   state: { messages: [] }
