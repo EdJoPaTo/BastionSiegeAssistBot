62783d768c8ec7c6b49a31b2f3936620cc402e66
Edgar Toll <edjopato@gmail.com>
2018-02-03 20:01:23 +0100

    feat: display resources needed

--- parts/bastionsiegeforward.js
+++ parts/bastionsiegeforward.js
@@ -1,7 +1,8 @@
 const Telegraf = require('telegraf')
 
 const { emoji, getScreenInformation } = require('../lib/gamescreen')
-const { calcMinutesNeededForUpgrade } = require('../lib/siegemath')
+const { formatNumberShort } = require('../lib/numberFunctions')
+const { calcBuildingCost, calcStorageCapacity, calcMinutesNeeded } = require('../lib/siegemath')
 
 const bot = new Telegraf.Composer()
 
@@ -18,12 +19,13 @@ bot.on('text', (ctx, next) => {
 const buildingsToShow = ['townhall', 'storage', 'houses', 'barracks', 'wall', 'trebuchet']
 
 bot.on('text', ctx => {
-  console.log('bastion siege message')
   const information = ctx.session.gameInformation
 
   if (!information.townhall) {
     return ctx.reply('please forward me the building screen from your game')
   }
+
+  const storageCapacity = calcStorageCapacity(information.storage)
 
   let text = ''
 
@@ -34,19 +36,37 @@ bot.on('text', ctx => {
       continue
     }
 
-    console.log(buildingName, information[buildingName], information.townhall, information.houses, information.sawmill, information.mine, information.gold, information.wood, information.stone)
-    const minutesNeeded = calcMinutesNeededForUpgrade(buildingName, information[buildingName], information.townhall, information.houses, information.sawmill, information.mine, information.gold, information.wood, information.stone)
-    if (minutesNeeded === 0) {
-      text += '✅'
-    } else {
-      text += `${minutesNeeded} minutes needed`
+    const cost = calcBuildingCost(buildingName, information[buildingName])
+    if (cost.wood > storageCapacity || cost.stone > storageCapacity) {
+      text += '⚠️ storage upgrade needed first\n'
+      continue
     }
-    text += '\n'
+
+    const minutesNeeded = calcMinutesNeeded(cost, information.townhall, information.houses, information.sawmill, information.mine, information.gold, information.wood, information.stone)
+    if (minutesNeeded === 0) {
+      text += '✅\n'
+      continue
+    }
+
+    const neededMaterialString = getNeededMaterialString(cost, information.gold, information.wood, information.stone)
+    text += `${minutesNeeded} minutes needed (${neededMaterialString})\n`
   }
 
   text += '\n\n🔜 in order to increase accuracy provide me updated resources or buildings of your game'
   return ctx.reply(text)
 })
+
+function getNeededMaterialString(cost, currentGold, currentWood, currentStone) {
+  const goldNeeded = cost.gold - currentGold
+  const woodNeeded = cost.wood - currentWood
+  const stoneNeeded = cost.stone - currentStone
+
+  const neededMaterial = []
+  if (goldNeeded > 0) { neededMaterial.push(`${formatNumberShort(goldNeeded, true)}${emoji.gold}`) }
+  if (woodNeeded > 0) { neededMaterial.push(`${formatNumberShort(woodNeeded, true)}${emoji.wood}`) }
+  if (stoneNeeded > 0) { neededMaterial.push(`${formatNumberShort(stoneNeeded, true)}${emoji.stone}`) }
+  return neededMaterial.join(', ')
+}
 
 const abstraction = new Telegraf.Composer()
 abstraction.use(Telegraf.optional(ctx => ctx && ctx.message && ctx.message.forward_from && ctx.message.forward_from.id === 252148344, bot))
