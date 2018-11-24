3f7de68ce91f21beb1f87db52536a42627455955
Edgar To <edjopato@gmail.com>
2018-11-25 00:53:42 +0100

    feat(playerstats): forward incoming attack to get playerstats

--- parts/playerstats.js
+++ parts/playerstats.js
@@ -7,21 +7,36 @@ const {createPlayerStatsString} = require('../lib/create-stats-strings')
 
 const bot = new Telegraf.Composer()
 
+function isAttackIncoming(ctx) {
+  return ctx.state.screen &&
+         ctx.state.screen.information &&
+         ctx.state.screen.information.attackincoming
+}
+
+bot.on('text', Telegraf.optional(isAttackIncoming, ctx => {
+  const {attackincoming} = ctx.state.screen.information
+  return sendPlayerStats(ctx, attackincoming.player)
+}))
+
 function isAttackScout(ctx) {
   return ctx.state.screen &&
          ctx.state.screen.information &&
          ctx.state.screen.information.attackscout
 }
 
-bot.on('text', Telegraf.optional(isAttackScout, async ctx => {
+bot.on('text', Telegraf.optional(isAttackScout, ctx => {
   const {attackscout} = ctx.state.screen.information
+  return sendPlayerStats(ctx, attackscout.player)
+}))
+
+async function sendPlayerStats(ctx, playername) {
   const allBattlereports = await battlereports.getAll()
-  const stats = playerStats.generate(allBattlereports, attackscout.player)
+  const stats = playerStats.generate(allBattlereports, playername)
 
   return ctx.replyWithMarkdown(
     createPlayerStatsString(allBattlereports, stats)
   )
-}))
+}
 
 module.exports = {
   bot
