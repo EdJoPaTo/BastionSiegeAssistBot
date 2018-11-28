11820f3395c97d3defb4f986fdab5b3553749ac7
Edgar To <edjopato@gmail.com>
2018-11-28 16:42:56 +0100

    refactor(playerstats): dont output the exact input in method

--- parts/bot-stats.js
+++ parts/bot-stats.js
@@ -9,15 +9,15 @@ const bot = new Telegraf.Composer()
 
 bot.command('botstats', async ctx => {
   const allBattlereports = await battlereports.getAll()
-  const {reports, enemies, immune} = playerStats.usageStats(allBattlereports)
+  const {enemies, immune} = playerStats.usageStats(allBattlereports)
 
   const minDate = getHoursEarlier(Date.now() / 1000, 24)
-  const reportsWithin24h = reports
+  const reportsWithin24h = allBattlereports
     .filter(o => o.time > minDate)
 
   let text = '*Statistics of the Bot*\n'
 
-  text += `\nBattlereports: ${reports.length}`
+  text += `\nBattlereports: ${allBattlereports.length}`
   text += `\nBattlereports added within 24h: ${reportsWithin24h.length}`
   text += `\nAnalysed Players: ${enemies.length}`
   text += `\nCurrently Immune 🛡💙: ${immune.length}`
