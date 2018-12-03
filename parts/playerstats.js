53c5afa0fce38e737e51288fed300374bf95ff65
Edgar To <edjopato@gmail.com>
2018-12-03 13:23:01 +0100

    fix(playerstats): only show stats for up to date messages
    incoming army and scouts could be send later in a bunch to get more
    playerstats at the same time. Only accept up to date ones.
    This stops crawlers from being effective.

--- parts/playerstats.js
+++ parts/playerstats.js
@@ -18,6 +18,13 @@ function isAttackIncoming(ctx) {
 
 bot.on('text', Telegraf.optional(isAttackIncoming, ctx => {
   const {attackincoming} = ctx.state.screen.information
+  const time = ctx.message.forward_date
+  const minutesAgo = ((Date.now() / 1000) - time) / 60
+  if (minutesAgo > 8) {
+    let text = ''
+    text += 'This battle is long over… Send me the report instead. 😉'
+    return ctx.reply(text)
+  }
   return sendPlayerStats(ctx, attackincoming.player)
 }))
 
@@ -29,6 +36,14 @@ function isAttackScout(ctx) {
 
 bot.on('text', Telegraf.optional(isAttackScout, ctx => {
   const {attackscout} = ctx.state.screen.information
+  const time = ctx.message.forward_date
+  const minutesAgo = ((Date.now() / 1000) - time) / 60
+  if (minutesAgo > 2) {
+    let text = ''
+    text += 'Your scouts havn\'t been there for a long time.'
+    text += ` I'll help you when you see ${attackscout.player} next time. 😊`
+    return ctx.reply(text)
+  }
   return sendPlayerStats(ctx, attackscout.player)
 }))
 
