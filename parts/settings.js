988d45b259930fb523fdf4d3c5dafb04afc07120
Edgar To <edjopato@gmail.com>
2018-12-08 22:17:51 +0100

    fixup! feat(buildings): show buildings based on user settings

--- parts/settings.js
+++ parts/settings.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf')
 const TelegrafInlineMenu = require('telegraf-inline-menu')
 
+const {emoji} = require('../lib/gamescreen.emoji')
 const {buildingNames, defaultBuildingsToShow} = require('../lib/buildings')
 
 const settingsMenu = new TelegrafInlineMenu('*Settings*')
@@ -9,28 +10,27 @@ settingsMenu.setCommand('settings')
 function buildingsText() {
   let text = '*Buildings*'
   text += '\nYou can set which buildings are of interest for you in the /buildings view.'
-
   return text
 }
+settingsMenu.submenu(emoji.houses + 'Buildings', 's', new TelegrafInlineMenu(buildingsText))
+  .select('b', buildingNames, {
+    multiselect: true,
+    columns: 2,
+    setFunc: (ctx, key) => {
+      ctx.session.buildings = toggleInArray(ctx.session.buildings || [...defaultBuildingsToShow], key)
+    },
+    isSetFunc: (ctx, key) => (ctx.session.buildings || [...defaultBuildingsToShow]).indexOf(key) >= 0
+  })
 
-const buildingsMenu = settingsMenu.submenu('Buildings', 's', new TelegrafInlineMenu(buildingsText))
-
-buildingsMenu.select('b', buildingNames, {
-  multiselect: true,
-  columns: 2,
-  setFunc: (ctx, key) => {
-    if (!ctx.session.buildings) {
-      ctx.session.buildings = [...defaultBuildingsToShow]
-    }
-    if (ctx.session.buildings.indexOf(key) >= 0) {
-      ctx.session.buildings = ctx.session.buildings.filter(o => o !== key)
-    } else {
-      ctx.session.buildings.push(key)
-      ctx.session.buildings.sort()
-    }
-  },
-  isSetFunc: (ctx, key) => (ctx.session.buildings || [...defaultBuildingsToShow]).indexOf(key) >= 0
-})
+function toggleInArray(array, key) {
+  if (array.indexOf(key) >= 0) {
+    array = array.filter(o => o !== key)
+  } else {
+    array.push(key)
+    array.sort()
+  }
+  return array
+}
 
 const bot = new Telegraf.Composer()
 bot.use(settingsMenu.init({
