2efc5817b145c39a0d5340da05916f898cb174b1
Edgar To <edjopato@gmail.com>
2018-12-14 02:03:53 +0100

    fixup! feat(battlereports): use new way to store battlereports

--- parts/battlestats.js
+++ parts/battlestats.js
@@ -15,10 +15,8 @@ function sendBattleStats(ctx) {
   const allReportsOfMyself = battlereports.getAll()
     .filter(o => o.providingTgUser === ctx.from.id)
   const firstTimeRelevant = getMidnightXDaysEarlier(Date.now() / 1000, 7)
-  const reportsFiltered = Object.keys(allReportsOfMyself)
-    .filter(key => Number(key) > firstTimeRelevant)
-    .map(key => allReportsOfMyself[key])
-    .filter(() => true)
+  const reportsFiltered = allReportsOfMyself
+    .filter(report => report.time > firstTimeRelevant)
 
   return ctx.replyWithMarkdown(
     createBattleStatsString(reportsFiltered)
