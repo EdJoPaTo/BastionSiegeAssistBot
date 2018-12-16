8ede11baf375d2611166edaf93779c13e89b219a
Edgar To <edjopato@gmail.com>
2018-12-16 05:26:06 +0100

    feat(alerts): show list of upcoming alerts with /upcoming

--- parts/alerts.js
+++ parts/alerts.js
@@ -4,6 +4,10 @@ const stringify = require('json-stable-stringify')
 const userSessions = require('../lib/data/user-sessions')
 
 const AlertHandler = require('../lib/user-interface/alert-handler')
+const {formatTimeAmount} = require('../lib/user-interface/format-number')
+
+const {alertEmojis} = AlertHandler
+const {Extra, Markup} = Telegraf
 
 const bot = new Telegraf.Composer()
 let alertHandler
@@ -33,6 +37,64 @@ function getCompareableString(ctx) {
     alerts,
     gameInformation
   })
+}
+
+bot.command('upcoming', ctx => {
+  const {text, extra} = generateUpcomingText(ctx)
+  return ctx.reply(text, extra)
+})
+
+bot.action('upcoming', ctx => {
+  const {text, extra} = generateUpcomingText(ctx)
+  return Promise.all([
+    ctx.answerCbQuery(),
+    ctx.editMessageText(text, extra)
+  ])
+})
+
+function generateUpcomingText(ctx) {
+  const enabledAlerts = ctx.session.alerts
+  const now = Date.now() / 1000
+  const eventList = alertHandler.generateUpcomingEventsList(ctx.session.gameInformation)
+    .filter(o => o.timestamp > now)
+    .sort((a, b) => a.timestamp - b.timestamp)
+
+  const entries = eventList
+    .map(event => {
+      let text = ''
+      text += '*'
+      text += enabledAlerts.indexOf(event.type) >= 0 ? alertEmojis.enabled : alertEmojis.disabled
+      text += alertEmojis[event.type]
+      text += ' '
+      text += formatTimeAmount((event.timestamp - now) / 60)
+      text += '*'
+      text += '\n'
+      text += new Date(event.timestamp * 1000).toISOString()
+      text += '\n'
+      text += event.text
+      return text
+    })
+
+  let text = '*Upcoming Alerts*'
+  text += '\nYou can select which alerts you want to receive in the /settings.'
+
+  text += '\n'
+  text += '\nnow: ' + new Date(now * 1000).toISOString()
+
+  text += '\n'
+  text += '\n' + entries.join('\n')
+  if (entries.length === 0) {
+    text += 'There are no upcoming events…'
+  }
+
+  const extra = Extra
+    .markdown()
+    .markup(
+      Markup.inlineKeyboard([
+        Markup.callbackButton('update', 'upcoming')
+      ])
+    )
+  return {text, extra}
 }
 
 module.exports = {
