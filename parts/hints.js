17cdd02a55d7b92d85b7bfca245de191690accb2
Edgar Toll <edjopato@gmail.com>
2019-02-14 20:29:05 +0100

    feat(poweruser): powerusers now have to specify their names

--- parts/hints.js
+++ parts/hints.js
@@ -7,11 +7,9 @@ const {emoji} = require('../lib/user-interface/output-text')
 const bot = new Telegraf.Composer()
 
 bot.on('text', async (ctx, next) => {
-  if (!ctx.session.disableImmunity &&
-    poweruser.isPoweruser(ctx.from.id) &&
+  if (poweruser.hasSendEnoughReports(ctx.from.id) &&
     !ctx.session.gameInformation.player) {
-    let text = ctx.i18n.t('poweruser.youare') + ' ' + emoji.poweruser
-    text += '\n⚠️ ' + ctx.i18n.t('poweruser.getImmunity')
+    const text = '\n⚠️' + emoji.poweruser + ' ' + ctx.i18n.t('poweruser.nameRequired')
     await ctx.replyWithMarkdown(text)
   }
 
