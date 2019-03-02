c102f1ef268cf3d28f0c5d2fd9657a55bb559f53
Edgar Toll <edjopato@gmail.com>
2019-03-02 21:58:49 +0100

    feat(hints): send name/buildings old hints

--- parts/hints.js
+++ parts/hints.js
@@ -1,5 +1,7 @@
 const Telegraf = require('telegraf')
 
+const {getMidnightXDaysEarlier} = require('../lib/math/unix-timestamp')
+
 const poweruser = require('../lib/data/poweruser')
 
 const {emoji} = require('../lib/user-interface/output-text')
@@ -10,9 +12,28 @@ const bot = new Telegraf.Composer()
 
 bot.on('text', async (ctx, next) => {
   const poweruserHints = []
+  const fiveDaysAgo = getMidnightXDaysEarlier(Date.now() / 1000, 5)
+  const sevenDaysAgo = getMidnightXDaysEarlier(Date.now() / 1000, 7)
+
   if (poweruser.hasSendEnoughReports(ctx.from.id)) {
     if (!ctx.session.gameInformation.player) {
       poweruserHints.push(ctx.i18n.t('poweruser.nameRequired'))
+    } else if (ctx.session.gameInformation.playerTimestamp < fiveDaysAgo) {
+      let text = ctx.i18n.t('poweruser.nameOld')
+      if (ctx.session.gameInformation.playerTimestamp > sevenDaysAgo) {
+        text += '\n' + ctx.i18n.t('poweruser.nearlyOld')
+      }
+
+      poweruserHints.push(text)
+    }
+
+    if (ctx.session.gameInformation.buildingsTimestamp < fiveDaysAgo) {
+      let text = ctx.i18n.t('poweruser.buildingsOld')
+      if (ctx.session.gameInformation.buildingsTimestamp > sevenDaysAgo) {
+        text += '\n' + ctx.i18n.t('poweruser.nearlyOld')
+      }
+
+      poweruserHints.push(text)
     }
   }
 
