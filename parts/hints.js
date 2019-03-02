089c82755c7db7be555278a5109c0dace6529073
Edgar Toll <edjopato@gmail.com>
2019-03-02 21:58:25 +0100

    refactor(hints): generalize hint sending

--- parts/hints.js
+++ parts/hints.js
@@ -4,13 +4,23 @@ const poweruser = require('../lib/data/poweruser')
 
 const {emoji} = require('../lib/user-interface/output-text')
 
+const POWERUSER_HINT_PREFIX = `⚠️${emoji.poweruser} `
+
 const bot = new Telegraf.Composer()
 
 bot.on('text', async (ctx, next) => {
-  if (!ctx.session.gameInformation.player &&
-    poweruser.hasSendEnoughReports(ctx.from.id)) {
-    const text = '\n⚠️' + emoji.poweruser + ' ' + ctx.i18n.t('poweruser.nameRequired')
-    await ctx.replyWithMarkdown(text)
+  const poweruserHints = []
+  if (poweruser.hasSendEnoughReports(ctx.from.id)) {
+    if (!ctx.session.gameInformation.player) {
+      poweruserHints.push(ctx.i18n.t('poweruser.nameRequired'))
+    }
+  }
+
+  const hints = [
+    ...poweruserHints.map(o => POWERUSER_HINT_PREFIX + o)
+  ]
+  if (hints.length > 0) {
+    await ctx.replyWithMarkdown(hints.join('\n\n'))
   }
 
   return next()
