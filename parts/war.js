15a5da5c9da9cabd1cf0a6c00190d0edee2eab07
Edgar Toll <edjopato@gmail.com>
2019-03-03 02:35:06 +0100

    refactor(war): move statsString generation into lib

--- parts/war.js
+++ parts/war.js
@@ -1,11 +1,10 @@
 const Telegraf = require('telegraf')
 
-const {calcBarracksCapacity} = require('../lib/math/siegemath')
-
 const playerStatsDb = require('../lib/data/playerstats-db')
 const poweruser = require('../lib/data/poweruser')
 
-const {createPlayerShareButton, createPlayerNameString, createPlayerStatsString, createTwoSidesStatsString} = require('../lib/user-interface/player-stats')
+const {createPlayerShareButton, createPlayerStatsString} = require('../lib/user-interface/player-stats')
+const {createWarStats} = require('../lib/user-interface/war-stats')
 const {emoji} = require('../lib/user-interface/output-text')
 const {formatNumberShort} = require('../lib/user-interface/format-number')
 
@@ -68,40 +67,12 @@ bot.on('text', Telegraf.optional(isWarMenu, ctx => {
         return ctx.replyWithMarkdown(text)
       }
 
-      const friends = battle.attack.includes(name) ? battle.attack : battle.defence
-      const poweruserFriends = poweruser.getPoweruserSessions()
-        .map(o => o.data.gameInformation)
-        .filter(o => o.player && friends.includes(o.player.name))
-        .filter(o => o.buildingsTimestamp > minimumBuildingTimestamp)
-        .map(o => ({
-          alliance: o.player.alliance,
-          player: o.player.name,
-          barracks: o.buildings.barracks,
-          army: calcBarracksCapacity(o.buildings.barracks)
-        }))
-
-      const additionalArmyInformation = {}
-      for (const o of poweruserFriends) {
-        additionalArmyInformation[o.player] = o.army
-      }
-
-      const notPowerusers = friends
-        .filter(o => !poweruserFriends.map(o => o.player).includes(o))
-      if (notPowerusers.length > 0) {
-        const notPoweruserString = notPowerusers
-          .map(o => createPlayerNameString({player: o}, true))
-          .join(', ')
-
-        text += ctx.i18n.t('battle.playersNotPowerusersOrBuildingsOld') + ':\n'
-        text += notPoweruserString
-        text += '\n\n'
-      }
+      text += createWarStats(time, battle, name)
 
       const attackStats = battle.attack
         .map(o => playerStatsDb.get(o))
       const defenceStats = battle.defence
         .map(o => playerStatsDb.get(o))
-      text += createTwoSidesStatsString(attackStats, defenceStats, additionalArmyInformation)
 
       const buttons = [...attackStats, ...defenceStats]
         .filter(o => !poweruser.isImmune(o.player))
