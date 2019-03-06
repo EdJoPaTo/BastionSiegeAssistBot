052138419997385801ee76b528d226c222a3cc80
Edgar Toll <edjopato@gmail.com>
2019-03-06 19:02:26 +0100

    feat(i18n): test for overspecified translations

---
+++ locales/locales.test.js
@@ -0,0 +1,12 @@
+import test from 'ava'
+import I18n from 'telegraf-i18n'
+
+const i18n = new I18n({
+  directory: 'locales'
+})
+
+for (const l of i18n.availableLocales()) {
+  test(`locale ${l} is not overspecified`, t => {
+    t.deepEqual(i18n.overspecifiedKeys(l), [])
+  })
+}
