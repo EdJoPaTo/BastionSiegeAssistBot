5b609db1bb145ab7b1cc1581f5e13f92051b4c75
Edgar Toll <edjopato@gmail.com>
2019-03-18 23:34:29 +0100

    fix(hints): only show hints once per 30 minutes

--- parts/hints.js
+++ parts/hints.js
@@ -6,12 +6,19 @@ const {emoji} = require('../lib/user-interface/output-text')
 const {getHintStrings} = require('../lib/user-interface/poweruser')
 
 const POWERUSER_HINT_PREFIX = emoji.poweruser
+const HINTS_MAX_INTERVAL = 60 * 30 // 30 Minutes
 
 const bot = new Telegraf.Composer()
 
 bot.on('text', async (ctx, next) => {
-  let poweruserHints = []
+  const now = Date.now() / 1000
+  if (ctx.session.lastHintTimestamp + HINTS_MAX_INTERVAL > now) {
+    return next()
+  }
 
+  ctx.session.lastHintTimestamp = now
+
+  let poweruserHints = []
   if (poweruser.hasSendEnoughReports(ctx.from.id)) {
     const conditions = poweruser.getConditions(ctx.from.id)
     poweruserHints = getHintStrings(ctx, conditions)
