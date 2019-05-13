8d33a94d7ecb83404b4141d5ea9c301861628623
Edgar Toll <edjopato@gmail.com>
2019-05-13 11:37:00 +0200

    feat(botstats): show analysed players within 30 days
    the rest has name changes and so on…

--- parts/bot-stats.js
+++ parts/bot-stats.js
@@ -6,7 +6,7 @@ const userSessions = require('../lib/data/user-sessions')
 
 const poweruser = require('../lib/data/poweruser')
 
-const {getHoursEarlier} = require('../lib/math/unix-timestamp')
+const {getHoursEarlier, getMidnightXDaysEarlier} = require('../lib/math/unix-timestamp')
 
 const {emoji} = require('../lib/user-interface/output-text')
 
@@ -18,15 +18,19 @@ bot.command('botstats', async ctx => {
   const users = userSessions.getRaw().length
   const powerusers = poweruser.getPoweruserSessions().length
 
-  const minDate = getHoursEarlier(Date.now() / 1000, 24)
+  const time24hAgo = getHoursEarlier(Date.now() / 1000, 24)
+  const date30dAgo = getMidnightXDaysEarlier(Date.now() / 1000, 30)
   const reportsWithin24h = allBattlereports
-    .filter(o => o.time > minDate)
+    .filter(o => o.time > time24hAgo)
+  const enemiesWithin30d = enemies
+    .filter(o => o.lastBattleTime > date30dAgo)
 
   let text = `*${ctx.i18n.t('botstats.title')}*\n`
 
   text += `\n${ctx.i18n.t('battlereports')}: ${allBattlereports.length}${emoji.battlereport}`
-  text += `\n${ctx.i18n.t('botstats.battlereportsWithin24h')}: ${reportsWithin24h.length}${emoji.battlereport}`
+  text += `\n  ${ctx.i18n.t('botstats.within24h')}: ${reportsWithin24h.length}${emoji.battlereport}`
   text += `\n${ctx.i18n.t('botstats.analysedPlayers')}: ${enemies.length}`
+  text += `\n  ${ctx.i18n.t('botstats.within30d')}: ${enemiesWithin30d.length}`
   text += `\n${ctx.i18n.t('botstats.users')}: ${users} (${powerusers}${emoji.poweruser})`
 
   return ctx.replyWithMarkdown(text)
