70670d66becea680ec273f72bbb3ca513bcd9b39
Edgar Toll <edjopato@gmail.com>
2019-11-03 17:54:35 +0100

    refactor(typescript): migrate part effects

---
+++ source/parts/effects.ts
@@ -0,0 +1,13 @@
+import {Composer} from 'telegraf'
+
+import {whenScreenContainsInformation} from '../lib/input/gamescreen'
+
+export const bot = new Composer()
+
+bot.on('text', whenScreenContainsInformation('effects', (ctx: any) => {
+  return ctx.reply(ctx.i18n.t('effects.updated'))
+}))
+
+module.exports = {
+  bot
+}
