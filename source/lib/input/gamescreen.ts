643479425e37f642de7798584ff5a0b6c307c7a8
Edgar Toll <edjopato@gmail.com>
2020-04-02 18:28:02 +0200

    refactor: simplify code

--- source/lib/input/gamescreen.ts
+++ source/lib/input/gamescreen.ts
@@ -1,18 +1,12 @@
 import {Gamescreen} from 'bastion-siege-logic'
 import Telegraf, {Middleware, ContextMessageUpdate} from 'telegraf'
-// TODO: wait for https://github.com/krmax44/always-array/pull/9
-// import alwaysArray, {SingleOrArray} from 'always-array'
 
 type GamescreenKey = keyof Gamescreen
-
 type SingleOrArray<T> = T | (readonly T[])
-function alwaysArray<T>(input: SingleOrArray<T>): readonly T[] {
-  return Array.isArray(input) ? input : [input]
-}
 
 // When at least one of the name argument is available middlewares are executed
 export function whenScreenContainsInformation(names: SingleOrArray<GamescreenKey>, ...middlewares: Middleware<ContextMessageUpdate>[]): (ctx: any, next: any) => void {
-  const namesArr = alwaysArray(names)
+  const namesArray = Array.isArray(names) ? names : [names]
   const predicate = (ctx: any): boolean => {
     if (!ctx.state.screen) {
       return false
@@ -20,14 +14,14 @@ export function whenScreenContainsInformation(names: SingleOrArray<GamescreenKey
 
     const screen: Gamescreen = ctx.state.screen || {}
     const available = Object.keys(screen) as GamescreenKey[]
-    return namesArr.some(n => available.includes(n))
+    return namesArray.some(n => available.includes(n))
   }
 
   return (Telegraf as any).optional(predicate, ...middlewares)
 }
 
-export function whenScreenIsOfType(wantedTypes: string | readonly string[], ...middlewares: Middleware<ContextMessageUpdate>[]): (ctx: any, next: any) => void {
-  const typeArr = Array.isArray(wantedTypes) ? wantedTypes : [wantedTypes]
+export function whenScreenIsOfType(wantedTypes: SingleOrArray<string>, ...middlewares: Middleware<ContextMessageUpdate>[]): (ctx: any, next: any) => void {
+  const typeArray = Array.isArray(wantedTypes) ? wantedTypes : [wantedTypes]
 
   const predicate = (ctx: any): boolean => {
     const screen = ctx.state.screen as Gamescreen
@@ -35,7 +29,7 @@ export function whenScreenIsOfType(wantedTypes: string | readonly string[], ...m
       return false
     }
 
-    return typeArr.some(n => screen.type === n)
+    return typeArray.some(n => screen.type === n)
   }
 
   return (Telegraf as any).optional(predicate, ...middlewares)
