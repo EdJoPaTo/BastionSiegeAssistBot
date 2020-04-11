bbbdc9589fd07bef3851a14d1875a111772dee70
EdJoPaTo <edjopato@gmail.com>
2020-04-11 15:02:48 +0200

    feat(castle): show more details to powerusers

--- source/lib/user-interface/castle-siege.ts
+++ source/lib/user-interface/castle-siege.ts
@@ -1,6 +1,21 @@
-import {Castle, castleGametext} from 'bastion-siege-logic'
+import {Castle, castleGametext, calcBarracksCapacity} from 'bastion-siege-logic'
+import arrayFilterUnique from 'array-filter-unique'
 
 import * as castles from '../data/castles'
+import * as castleSiege from '../data/castle-siege'
+import * as playerStatsDb from '../data/playerstats-db'
+import * as userSessions from '../data/user-sessions'
+
+import {createMultipleStatsConclusion, createPlayerStatsSingleLineString} from './player-stats'
+import {getMissingAllianceMates, createMissingMatesMarkdownList} from './alliance'
+
+export interface CastlePartOptions {
+  readonly userIsPoweruser: boolean;
+  readonly userAlliance: string | undefined;
+  readonly locale?: string;
+  readonly timeZone?: string;
+  readonly now: number;
+}
 
 function castleFormattedTimestampBegin(castle: Castle, locale: string | undefined, timeZone: string | undefined): string {
   return new Date(castles.nextSiegeAvailable(castle) * 1000).toLocaleString(locale, {
@@ -23,7 +38,7 @@ function castleFormattedTimestampEnd(castle: Castle, locale: string | undefined,
   })
 }
 
-export function castleHeader(castle: Castle, currentKeeper: string | undefined, locale: string | undefined, timeZone: string | undefined): string {
+function castleHeader(castle: Castle, currentKeeper: string | undefined, locale: string | undefined, timeZone: string | undefined): string {
   let text = ''
   if (currentKeeper) {
     text += currentKeeper
@@ -38,4 +53,77 @@ export function castleHeader(castle: Castle, currentKeeper: string | undefined, 
   text += castleFormattedTimestampEnd(castle, locale, timeZone)
   text += '\n'
   return text
+}
+
+function otherParticipantsArmyEstimation(participatingAlliances: readonly string[]): string {
+  const text = participatingAlliances
+    .map(o => playerStatsDb.getInAlliance(o, 7))
+    .filter(o => o.length >= 5)
+    .map(o => createMultipleStatsConclusion(o))
+    .map(o => o.armyString)
+    .join('\n')
+
+  return text + '\n'
+}
+
+export function castlePart(castle: Castle, options: CastlePartOptions): string {
+  const {userIsPoweruser, userAlliance, locale, timeZone, now} = options
+  let part = ''
+
+  const keeper = castles.currentKeeperAlliance(castle)
+  part += castleHeader(castle, keeper, locale, timeZone)
+
+  if (!castles.isCurrentlySiegeAvailable(castle, now) || !userIsPoweruser) {
+    return part
+  }
+
+  const participatingAlliances = [
+    keeper,
+    ...castleSiege.getAlliances(castle, now)
+  ].filter(o => o).filter(arrayFilterUnique()) as string[]
+
+  part += otherParticipantsArmyEstimation(participatingAlliances.filter(o => o !== userAlliance))
+
+  if (userAlliance && participatingAlliances.includes(userAlliance)) {
+    part += '\n'
+    const participants = castleSiege.getParticipants(castle, userAlliance, now)
+      .map(o => playerStatsDb.get(o.player))
+
+    const missingMates = getMissingAllianceMates(userAlliance, participants.map(o => o.player))
+    const missingEntries = createMissingMatesMarkdownList(missingMates)
+    if (missingEntries.length > 0) {
+      part += userAlliance + ' '
+      part += `Missing (${missingEntries.length}): `
+      part += missingEntries
+        .join(', ')
+      part += '\n\n'
+    }
+
+    const knownArmiesArray = userSessions.getRawInAlliance(userAlliance)
+      .map(o => o.data.gameInformation)
+      .filter(o => o.player && o.buildings)
+      .map(o => ({
+        player: o.player!.name,
+        army: calcBarracksCapacity(o.buildings!.barracks)
+      }))
+    const knownArmies: Record<string, number> = {}
+    for (const o of knownArmiesArray) {
+      knownArmies[o.player] = o.army
+    }
+
+    part += createMultipleStatsConclusion(participants, knownArmies).armyString
+    part += '\n'
+
+    part += participants
+      .map(stats => {
+        const userId = userSessions.getUserIdByName(stats.player)
+        const armyOverride = knownArmies[stats.player]
+        return createPlayerStatsSingleLineString(stats, userId, armyOverride)
+      })
+      .join('\n')
+
+    part += '\n'
+  }
+
+  return part
 }
--- source/parts/castle-siege.ts
+++ source/parts/castle-siege.ts
@@ -1,23 +1,18 @@
 import {Composer, ContextMessageUpdate} from 'telegraf'
 import {CASTLES, CASTLE_HOLD_SECONDS} from 'bastion-siege-logic'
-import arrayFilterUnique from 'array-filter-unique'
 import debounce from 'debounce-promise'
 
 import {whenScreenContainsInformation, whenScreenIsOfType} from '../lib/input/gamescreen'
 
 import {Session} from '../lib/types'
 
-import * as castles from '../lib/data/castles'
 import * as castleSiege from '../lib/data/castle-siege'
 import * as poweruser from '../lib/data/poweruser'
-import * as userSessions from '../lib/data/user-sessions'
 
 import {notNewMiddleware} from '../lib/telegraf-middlewares'
 
-import {castleHeader} from '../lib/user-interface/castle-siege'
-import {createPlayerMarkdownLink, createPlayerNameString} from '../lib/user-interface/player-stats'
+import {castlePart, CastlePartOptions} from '../lib/user-interface/castle-siege'
 import {emoji} from '../lib/user-interface/output-text'
-import {getMissingAllianceMateMarkdownList} from '../lib/user-interface/alliance'
 
 const DEBOUNCE_TIME = 200 // Milliseconds
 
@@ -39,58 +34,16 @@ bot.command('castle', async ctx => {
     text += '\n\n'
   }
 
-  const {alliance} = gameInformation.player || {}
+  const options: CastlePartOptions = {
+    userAlliance: gameInformation.player?.alliance,
+    locale: lang,
+    now,
+    timeZone,
+    userIsPoweruser
+  }
 
   const castleParts = CASTLES
-    .map(castle => {
-      const keeper = castles.currentKeeperAlliance(castle)
-
-      let part = ''
-      part += castleHeader(castle, keeper, lang, timeZone)
-
-      if (castles.isCurrentlySiegeAvailable(castle, now)) {
-        const participatingAlliances = [
-          keeper,
-          ...castleSiege.getAlliances(castle, now)
-        ].filter(o => o).filter(arrayFilterUnique()) as string[]
-        const onlyAttackers = participatingAlliances
-          .filter(o => o !== keeper)
-        if (onlyAttackers.length > 0) {
-          part += (ctx as any).i18n.t('bs.siege')
-          part += ': '
-          part += onlyAttackers.join('')
-          part += '\n'
-        }
-
-        if (userIsPoweruser && alliance && participatingAlliances.includes(alliance)) {
-          const participants = castleSiege.getParticipants(castle, alliance, now)
-
-          const missing = getMissingAllianceMateMarkdownList(alliance, participants.map(o => o.player))
-          if (missing.length > 0) {
-            part += alliance + ' '
-            part += `Missing (${missing.length}): `
-            part += missing
-              .join(', ')
-            part += '\n'
-          }
-
-          part += alliance + ' '
-          part += `Participants (${participants.length}): `
-          part += participants
-            .map(o => {
-              const playerWithoutAlliance = {player: o.player}
-              const userId = userSessions.getUserIdByName(o.player)
-              return userId ?
-                createPlayerMarkdownLink(userId, playerWithoutAlliance) :
-                createPlayerNameString(playerWithoutAlliance, true)
-            })
-            .join(', ')
-          part += '\n'
-        }
-      }
-
-      return part
-    })
+    .map(castle => castlePart(castle, options))
 
   text += castleParts.join('\n\n')
 
