4273b5ea4ea11a813079672a392278f873a1b69a
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-18 00:02:45 +0200

    feat: allow old messages from powerusers
    they can look up stuff anyway

--- source/lib/telegraf-middlewares.ts
+++ source/lib/telegraf-middlewares.ts
@@ -1,10 +1,13 @@
 import {Context as TelegrafContext, Middleware} from 'telegraf'
 
+import {isPoweruser} from './data/poweruser'
+
 export function notNewMiddleware(i18nMessage = 'forward.old', maxAgeInMinutes = 8): Middleware<TelegrafContext> {
   return async (ctx, next) => {
+    const fromId = ctx.from!.id
     const time = ctx.message!.forward_date!
     const minutesAgo = ((Date.now() / 1000) - time) / 60
-    if (minutesAgo > maxAgeInMinutes) {
+    if (minutesAgo > maxAgeInMinutes && !isPoweruser(fromId)) {
       await ctx.reply((ctx as any).i18n.t(i18nMessage))
       return
     }
